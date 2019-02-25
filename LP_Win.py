be020f0d88f6be652393ec6a24166dfbc5a88753
Spaceybread <Spaceybread@gmail.com>
2019-02-25 13:10:34 +0530

    Allows the average data instead of a single match

---
+++ LP_Win.py
@@ -0,0 +1,18 @@
+played = input('How many games has this team played?  ')
+won = input('How many gamees has this team played?  ')
+p = int(played)
+w = int(won)
+gold = input('How much gold does this acquire on average?  ')
+g = int(gold)
+kills = input("How many kills take place on an average in this team's games?  ")
+executions = input("How many kills does this team execute?  ")
+k = int(kills)
+e = int(executions)
+barons = input("How many barons are killed in their game on an average?  ")
+baron = input("How many barons does this team kill on an average?  ")
+bs = int(barons)
+b = int(baron)
+
+winscore = ((w/p)*100) + (g/1000) + ((e/k)*100) + ((b/bs)*100)
+
+print("This team has a score of {}".format(winscore))
